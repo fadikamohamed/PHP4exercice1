09ed039b4eb359c6d57c41812cd2f7ed249d6487
fadikamohamed <fadika.mohamed02@gmail.com>
2018-09-03 14:59:00 +0200

    exercice terminé

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title></title>
+  </head>
+  <body>
+    <p>
+    <?php
+      function returnTrue() {
+        echo 'true';
+      }
+      returnTrue();
+     ?>
+   </p>
+  </body>
+</html>
